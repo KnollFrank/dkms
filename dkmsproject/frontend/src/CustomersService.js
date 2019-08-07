2a6dfe6490b40e039c89a91a74988cc3c46ec267
Frank Knoll <Knoll_Frank@web.de>
2019-08-07 13:48:54 +0200

    refactoring

--- dkmsproject/frontend/src/CustomersService.js
+++ dkmsproject/frontend/src/CustomersService.js
@@ -3,33 +3,8 @@ const API_URL = 'http://localhost:8000';
 
 export default class CustomersService{
 
-    getCustomers() {
-        const url = `${API_URL}/api/backend/`;
-        return axios.get(url).then(response => response.data);
-    }
-
-    getCustomersByURL(link){
-        const url = `${API_URL}${link}`;
-        return axios.get(url).then(response => response.data);
-    }
-
-    getCustomer(pk) {
-        const url = `${API_URL}/api/backend/${pk}`;
-        return axios.get(url).then(response => response.data);
-    }
-
-    deleteCustomer(customer){
-        const url = `${API_URL}/api/backend/${customer.pk}`;
-        return axios.delete(url);
-    }
-
     createCustomer(customer){
         const url = `${API_URL}/api/backend/`;
         return axios.post(url,customer);
-    }
-
-    updateCustomer(customer){
-        const url = `${API_URL}/api/backend/${customer.pk}`;
-        return axios.put(url,customer);
     }
 }
