6ace630ce12c6a0809f9ec0a216e9d96ccaebf10
Frank Knoll <Knoll_Frank@web.de>
2019-08-07 12:51:06 +0200

    refining tests

--- dkmsproject/backend/tests.py
+++ dkmsproject/backend/tests.py
@@ -1,21 +1,27 @@
 from django.test import TestCase
-
 from .models import Customer
+# from pprint import pprint
 
-def create_data(id):
+def create_customer(id, first_name):
     return Customer.objects.create(
         id=id,
-        first_name="Customer first_name",
+        first_name=first_name,
         last_name="Customer last_name",
         email="customer001@email.com",
         phone="00000000",
         address="Customer 000 Address",
         description= "Customer 001 description")
 
+class CustomerTests(TestCase):
 
-class CustomerDetailViewTests(TestCase):
-    def test_customer(self):
-        id = 4711
-        customer = create_data(id)
-        response = self.client.get('/api/backend/' + str(id))
-        self.assertContains(response, customer.first_name)
+    def test_getCustomer(self):
+        # Given
+        customer = create_customer(id=4711, first_name="some first name")
+
+        # When
+        response = self.client.get('/api/backend/' + str(customer.id))
+
+        # Then
+        # pprint(response.__dict__)
+        self.assertEquals(response.data['pk'], customer.id)
+        self.assertEquals(response.data['first_name'], customer.first_name)
