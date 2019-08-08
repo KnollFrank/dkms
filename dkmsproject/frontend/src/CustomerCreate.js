e86d6d2c97e6932a36f51ff43a862c12e4a51b04
Frank Knoll <Knoll_Frank@web.de>
2019-08-08 14:42:27 +0200

    refactoring

--- dkmsproject/frontend/src/CustomerCreate.js
+++ dkmsproject/frontend/src/CustomerCreate.js
@@ -13,6 +13,7 @@ export default class CustomerCreate extends React.Component {
         description: ''
       };
       this.submit = this.submit.bind(this);
+      this.handleUserInput = this.handleUserInput.bind(this);
     }
 
     submit(e) {
@@ -25,6 +26,12 @@ export default class CustomerCreate extends React.Component {
         }
     }
 
+    handleUserInput(e) {
+      const name = e.currentTarget.name;
+      const value = e.target.value;
+      this.setState({[name]: value});
+    }
+
     render() {
       return (
         <form>
@@ -35,7 +42,7 @@ export default class CustomerCreate extends React.Component {
               type="text"
               name="first_name"
               value={this.state.first_name}
-              onChange={e => this.setState({ first_name: e.target.value })}
+              onChange={this.handleUserInput}
             />
 
             <label>Last Name:</label>
@@ -44,7 +51,7 @@ export default class CustomerCreate extends React.Component {
               type="text"
               name="last_name"
               value={this.state.last_name}
-              onChange={e => this.setState({ last_name: e.target.value })}
+              onChange={this.handleUserInput}
             />
 
             <label>Email:</label>
@@ -53,7 +60,7 @@ export default class CustomerCreate extends React.Component {
               type="text"
               name="email"
               value={this.state.email}
-              onChange={e => this.setState({ email: e.target.value })}
+              onChange={this.handleUserInput}
             />
 
             <label>Phone:</label>
@@ -62,7 +69,7 @@ export default class CustomerCreate extends React.Component {
               type="text"
               name="phone"
               value={this.state.phone}
-              onChange={e => this.setState({ phone: e.target.value })}
+              onChange={this.handleUserInput}
             />
 
             <label>Address:</label>
@@ -71,7 +78,7 @@ export default class CustomerCreate extends React.Component {
               type="text"
               name="address"
               value={this.state.address}
-              onChange={e => this.setState({ address: e.target.value })}
+              onChange={this.handleUserInput}
             />
 
             <label>Description:</label>
@@ -79,7 +86,7 @@ export default class CustomerCreate extends React.Component {
               className="form-control"
               name="description"
               value={this.state.description}
-              onChange={e => this.setState({ description: e.target.value })}
+              onChange={this.handleUserInput}
             />
 
             <button className="btn btn-primary" onClick={this.submit}>Submit</button>
