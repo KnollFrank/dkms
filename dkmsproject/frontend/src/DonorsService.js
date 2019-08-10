3e0bbae545f30714a13a802aa35c4ac2f5337d08
Frank Knoll <Knoll_Frank@web.de>
2019-08-10 20:19:42 +0200

    renaming

--- dkmsproject/frontend/src/DonorsService.js
+++ dkmsproject/frontend/src/DonorsService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 const API_URL = 'http://localhost:8000';
 
-export default class CustomersService{
+export default class DonorsService {
 
     createCustomer(customer){
         const url = `${API_URL}/api/backend/`;
