746a5d6d391fcef27e1c674ad50edb9be98cfdaa
Frank Knoll <Knoll_Frank@web.de>
2019-08-12 17:33:53 +0200

    refactoring by letting the frontend display the ancestry_choices from the rest api.

--- dkmsproject/frontend/src/DonorsService.js
+++ dkmsproject/frontend/src/DonorsService.js
@@ -3,8 +3,15 @@ const API_URL = 'http://localhost:8000';
 
 export default class DonorsService {
 
-    createDonor(donor){
+    createDonor(donor) {
         const url = `${API_URL}/api/backend/`;
         return axios.post(url, donor);
+    }
+
+    get_ancestry_choices() {
+      const url = `${API_URL}/api/backend/ancestry_choices`;
+      return axios
+        .get(url)
+        .then(response => { return response.data; })
     }
 }
